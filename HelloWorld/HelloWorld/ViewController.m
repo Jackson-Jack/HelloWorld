6743dddf31b38fe4c53eaaf6c9981dbf8a00529c
daiqingbo <Dqb169@163.com>
2015-09-01 20:03:40 +0800

    commit a change maybe counter conflict

--- HelloWorld/HelloWorld/ViewController.m
+++ HelloWorld/HelloWorld/ViewController.m
@@ -18,14 +18,26 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     
     UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 100, 44)];
     titleLabel.text = @"Test";
     titleLabel.font = [UIFont systemFontOfSize:14];
-    titleLabel.backgroundColor = [UIColor lightGrayColor];
     titleLabel.backgroundColor = [UIColor clearColor];
+    titleLabel.textColor = [UIColor blackColor];
     [self.view addSubview:titleLabel];
+    
+    UIButton *sendButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [sendButton setBackgroundColor:[UIColor lightGrayColor]];
+    sendButton.frame = CGRectMake(100, 200, 100, 44);
+    [sendButton setTitle:@"测试" forState:UIControlStateNormal];
+    [self.view addSubview:sendButton];
+    [sendButton addTarget:self action:@selector(sendButtonClicked) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)sendButtonClicked
+{
+    NSLog(@"sendButtonClicked");
 }
 
 - (void)didReceiveMemoryWarning {
