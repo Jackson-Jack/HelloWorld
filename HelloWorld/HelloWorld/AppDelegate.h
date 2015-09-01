b84cc1c2e4f2b04c3de973e72876dcf85f11b857
daiqingbo <Dqb169@163.com>
2015-09-01 16:20:21 +0800

    create a new project

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by DQB on 15/9/1.
+//  Copyright (c) 2015年 DQB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by DQB on 15/9/1.
+//  Copyright (c) 2015年 DQB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 100, 44)];
+    titleLabel.text = @"Test";
+    [self.view addSubview:titleLabel];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
