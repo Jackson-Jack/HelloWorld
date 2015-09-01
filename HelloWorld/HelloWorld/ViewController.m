bbe811581e17c3235ff1b4ee09b38bf7fc9f0b8c
Jackson <dqb1690@163.com>
2015-09-01 19:14:02 +0800

    test diff

--- HelloWorld/HelloWorld/ViewController.m
+++ HelloWorld/HelloWorld/ViewController.m
@@ -21,7 +21,7 @@
     self.view.backgroundColor = [UIColor redColor];
     
     UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 100, 44)];
-    titleLabel.text = @"Test";
+    titleLabel.text = @"abc";
     [self.view addSubview:titleLabel];
 }
 
